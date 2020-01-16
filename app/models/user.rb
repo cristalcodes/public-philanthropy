8e9a17941267c5044dbff57bb0fff9832238ce2a
Cristal Alatorre Perez <cristalalatorreperez@albert.home>
2020-01-16 15:48:33 -0800

    Adds first and last name, email to Users table. Adds .from_omniauth method to User model, as well as the Callbacks controller to devise_for users. Defines devise_scope.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.phone = auth.info.phone
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+
 end
