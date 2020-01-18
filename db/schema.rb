8611ddb898cc6e11bb3569d547f52fa4f77c3e8f
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-01-17 17:08:40 -0800

    Adds date attribute to donations table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_171557) do
+ActiveRecord::Schema.define(version: 2020_01_18_010250) do
 
   create_table "donations", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_171557) do
     t.text "comments"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date"
   end
 
   create_table "organizations", force: :cascade do |t|
