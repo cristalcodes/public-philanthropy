06895e4e5340857395c4a7f1df541268e75357fb
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-01-17 19:10:04 -0800

    Adds strong params to Organization controller.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,13 @@
 class OrganizationsController < ApplicationController
 
   def new
-    @organization = Organization.new 
+    @organization = Organization.new
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(:name)
   end
 
 
